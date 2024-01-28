7addb3a54290bb01e75613d87d9cfa4b55a83c4e
Alanna DEVLIN GENIN <alannadevlingenin@gmail.com>
2024-01-29 00:33:51 +0100

    [QUIZZ-20] Create tests for helper functions (#8)
    * test(helpers): test encoding string
    * build(pyproject): add src folder for pytest
    * build(pyproject): exclude tests from bandit analysis
    * test(quizzify): change folder structure

---
+++ tests/quizzify/quiz/utils/test_helpers.py
@@ -0,0 +1,10 @@
+from src.quizzify.quiz.utils.helpers import encode_str_to_base64
+
+
+def test_encode_str_to_base64():
+    """Test encode_str_to_base64() helper function."""
+    string_to_encode = "test"
+    encoded_string = encode_str_to_base64(string_to_encode)
+    assert isinstance(encoded_string, str)
+    assert encoded_string == "dGVzdA=="
+    assert string_to_encode != encoded_string
