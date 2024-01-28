f39160aca2293548501881014dc3ea5add2ad7dc
Alanna DEVLIN GENIN <alannadevlingenin@gmail.com>
2024-01-28 23:55:05 +0100

    feat(logging): update back-end logs configuration

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-"""Quizzify package."""
+"""Quizzify package.
+
+This package contains the Quizzify API. The API is built using FastAPI and
+uses the Spotify API to retrieve information about the user's Spotify account
+and generate a music quiz.
+"""
